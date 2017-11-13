b0bae032b81d642d4070ec47a2d1bacbc2a659bd
Sean Lee <15sl18@caslab.queensu.ca>
2017-11-13 13:06:57 -0500

    tower constructor

--- GUI/GUI/Tower.cpp
+++ GUI/GUI/Tower.cpp
@@ -2,3 +2,16 @@
 #include "Tower.h"
 #include <SFML/Graphics.hpp>
 #include "stdafx.h"
+#include <fstream>
+#include "BuySell.h"
+
+Tower::Tower(int type, buySell overlay) {
+	towerType = type;
+	switch (type) {
+	case 1: towerImage = overlay.getTower(1);
+		case 2: towerImage = overlay.getTower(2);
+		case 3: towerImage = overlay.getTower(3);
+	}
+	fstream towerStat;
+	towerStat.open("statSheet.txt");
+}
--- GUI/GUI/Tower.h
+++ GUI/GUI/Tower.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "stdafx.h"
+#include "BuySell.h"
 class Tower {
 private:
 
 	sf::Sprite towerImage;
 	int x;
 	int y;
-	int type;
+	int towerType;
 	int damage;
 	int attackSpeed;
 	int cost;
@@ -15,9 +16,7 @@ private:
 
 public:
 
-	Tower(sf::Sprite img) {
-		towerImage = img;
-	}
+	Tower(int type, buySell overlay);
 
 	int getX() {
 		return x;
@@ -25,7 +24,9 @@ public:
 	int getY() {
 		return y;
 	}
-
+	int getCost() {
+		return cost;
+	}
 	sf::Sprite getSprite() {
 		return towerImage;
 	}
